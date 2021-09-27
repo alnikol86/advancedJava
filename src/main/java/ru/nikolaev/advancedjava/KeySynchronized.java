d5589918aecdc54d4ee642ec152d46a7736230cc
Alexander Nikolaev <nikolaevairman@gmail.com>
2021-09-27 22:44:19 +0300

    Keyword synchronized. Race condition

---
+++ src/main/java/ru/nikolaev/advancedjava/KeySynchronized.java
@@ -0,0 +1,40 @@
+package main.java.ru.nikolaev.advancedjava;
+
+public class KeySynchronized {
+    private int counter;
+
+    public static void main(String[] args) throws InterruptedException {
+        KeySynchronized keySynchronized = new KeySynchronized();
+        keySynchronized.doWork();
+    }
+
+    public synchronized void increment() {
+        counter++;
+    }
+
+    public void doWork() throws InterruptedException {
+        Thread myThread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++)
+                    increment();
+            }
+        });
+
+        Thread myThread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++)
+                    increment();
+            }
+        });
+
+        myThread1.start();
+        myThread2.start();
+
+        myThread1.join();
+        myThread2.join();
+
+        System.out.println(counter);
+    }
+}
