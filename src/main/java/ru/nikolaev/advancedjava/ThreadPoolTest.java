fd9713dc73601d9753e44f41509efc8d829f3858
Alexander Nikolaev <nikolaevairman@gmail.com>
2021-09-28 22:11:21 +0300

    Thread pool. ExecutorService.

---
+++ src/main/java/ru/nikolaev/advancedjava/ThreadPoolTest.java
@@ -0,0 +1,36 @@
+package main.java.ru.nikolaev.advancedjava;
+
+import java.util.concurrent.*;
+
+public class ThreadPoolTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        for (int i = 0; i < 5; i++)
+            executorService.submit(new Work(i));
+
+        executorService.shutdown();
+        System.out.println("All tasks submitted.");
+
+        executorService.awaitTermination(1, TimeUnit.DAYS);
+    }
+}
+
+class Work implements Runnable {
+    private int id;
+
+    public Work(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Work " + id + " was completed.");
+    }
+}
