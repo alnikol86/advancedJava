b416602681785760a15d0e1e864a25d9ba9e22a2
Alexander Nikolaev <nikolaevairman@gmail.com>
2021-10-07 23:16:31 +0300

    Wait and Notify

---
+++ src/main/java/ru/nikolaev/advancedjava/WaitAndNotify.java
@@ -0,0 +1,61 @@
+package main.java.ru.nikolaev.advancedjava;
+
+import java.security.PrivilegedAction;
+import java.util.Scanner;
+
+public class WaitAndNotify {
+    public static void main(String[] args) throws InterruptedException {
+        WaitAndNotifyTest wn = new WaitAndNotifyTest();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    wn.produce();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    wn.consume();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+    }
+}
+
+class WaitAndNotifyTest {
+    public void produce() throws InterruptedException {
+        synchronized (this) {
+            System.out.println("Producer thread started...");
+            wait(); // 1 - отдаём intrinsic lock, 2 - ждём пока будет вызван notify();
+            System.out.println("Producer thread resumed...");
+        }
+    }
+
+    public void consume() throws InterruptedException {
+        Thread.sleep(2000);
+        Scanner scanner = new Scanner(System.in);
+
+        synchronized (this) {
+            System.out.println("Waiting for return key pressed");
+            scanner.nextLine();
+            notify(); //не отдаёт intrinsic lock
+
+            Thread.sleep(5000);
+        }
+    }
+}
